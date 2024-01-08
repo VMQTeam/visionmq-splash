a4f3e6fe6e6c6988a4db2b605dd9b3330b76533f
JReyCodes <121682721+JReyCodes@users.noreply.github.com>
2024-01-08 10:16:46 -0500

    removed the app is still working heading

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -9,7 +9,6 @@ const Header = () => {
     <div id='header'>
     <div id='header1'>
       <a href='/'><img src='https://images2.imgbox.com/43/cd/7tz2s2d1_o.png' alt="Vision MQ logo" id="logo" /></a>
-      <h1>App is working still</h1>
     </div>
       <div id='header2'>
         <Link to='/team'><button id='team-button'>team</button></Link>
